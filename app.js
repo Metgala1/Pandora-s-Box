5435f05e3be97fe6ce7f041eeb41afc8c8d06cc8
ATAGWE ROGER <atagweroger2@gmail.com>
2025-09-05 05:13:32 +0100

    Change in origin

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://pandora-s-box-theta.vercel.app/"],
+    origin: ["http://localhost:5173", "https://pandora-s-box-theta.vercel.app"],
     credentials: true,
   })
 );
