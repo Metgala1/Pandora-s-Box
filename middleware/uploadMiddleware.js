87d9cf03e2fb0089635f62bb711f41dfb8c778fc
ATAGWE ROGER <atagweroger2@gmail.com>
2025-09-06 10:14:56 +0100

    Dev commit

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -5,12 +5,6 @@ const fs = require("fs");
 // Define the uploads folder
 const uploadPath = path.join(__dirname, "../uploads");
 
-// Ensure the uploads folder exists
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
-
-// Multer storage configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, uploadPath);
